98ee966504203fa1f432e1a871f2be8e12f83343
Egor Pozdnov <pozdner98@gmail.com>
2025-10-24 10:20:30 +0300

    исправление bank account

--- bankAccount/src/main/java/org/example/BankAccount.java
+++ bankAccount/src/main/java/org/example/BankAccount.java
@@ -8,14 +8,30 @@ public class BankAccount {
     }
 
     public void deposit(double amount) {
+        if (amountIsNegative(amount)) {
+            throw new IllegalArgumentException("Сумма пополнения должна быть больше 0");
+        }
+
         this.balance += amount;
     }
 
     public void withdraw(double amount) {
+        if (amountIsNegative(amount)) {
+            throw new IllegalArgumentException("Сумма списания должна быть больше 0");
+        }
+
+        if (amount > this.balance) {
+            return;
+        }
+
         this.balance -= amount;
     }
 
     public double getBalance() {
         return this.balance;
+    }
+
+    private boolean amountIsNegative(double amount) {
+        return amount >= 0;
     }
 }
